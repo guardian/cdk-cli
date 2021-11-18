6fe207942fd09fd5278c80885f3cd0abf3f616d8
Akash Askoolum <akash1810@gmail.com>
2021-11-18 16:33:16 +0000

    refactor!: Remove parameter input
    Our migration strategy has changed.
    
    Best practice now is to use a `CfnInclude` construct to adopt the YAML template.
    
    We no longer need to:
      - parse the YAML file
      - accept user input that describes parameters in the YAML file
    
    BREAKING CHANGE: YAML template parameters can no longer be input

--- src/utils/stack.ts
+++ src/utils/stack.ts
@@ -1,20 +1,9 @@
-import type { CfnParameterProps } from "@aws-cdk/core";
-import { CodeMaker, toCamelCase } from "codemaker";
+import { CodeMaker } from "codemaker";
 import type { Imports } from "./imports";
 import type { Name } from "./utils";
 
-interface CdkParameterProps extends CfnParameterProps {
-  parameterType: string;
-  comment?: string;
-}
-
-export interface StackTemplate {
-  Parameters: Record<string, CdkParameterProps>;
-}
-
 interface StackBuilderProps {
   imports: Imports;
-  template: StackTemplate;
   appName: Name;
   outputFile: string;
   outputDir: string;
@@ -24,14 +13,11 @@ interface StackBuilderProps {
 export class StackBuilder {
   config: StackBuilderProps;
   imports: Imports;
-  template: StackTemplate;
-
   code: CodeMaker;
 
   constructor(props: StackBuilderProps) {
     this.config = props;
     this.imports = props.imports;
-    this.template = props.template;
 
     this.code = new CodeMaker({ indentationLevel: 2 });
     this.code.closeBlockFormatter = (s?: string): string => s ?? "}";
@@ -54,89 +40,11 @@ export class StackBuilder {
     );
     this.code.line("super(scope, id, props);");
 
-    this.addParams();
-
     this.code.closeBlock();
     this.code.closeBlock();
 
     this.code.closeFile(this.config.outputFile);
     await this.code.save(this.config.outputDir);
-  }
-
-  addParams(): void {
-    if (!Object.keys(this.template.Parameters).length) return;
-
-    this.code.line();
-    this.code.line("/* Parameters */");
-
-    this.code.line(
-      "// TODO: Consider if any of the parameter constructs from @guardian/cdk could be used here"
-    );
-    this.code.line(
-      "// https://github.com/guardian/cdk/blob/main/src/constructs/core/parameters.ts"
-    );
-    this.code.openBlock(`const parameters =`);
-
-    Object.keys(this.template.Parameters).forEach((paramName) => {
-      this.addParam(paramName, this.template.Parameters[paramName]);
-    });
-
-    this.code.closeBlock("};");
-  }
-
-  addParam(name: string, props: CdkParameterProps): void {
-    if (props.comment) {
-      this.code.line(`// ${props.comment}`);
-    }
-
-    const propsToRender = Object.entries(props).filter(
-      ([key, val]) => !this.shouldSkipParamProp(key, val)
-    );
-
-    if (!propsToRender.length) {
-      this.code.line(
-        `${name}: new ${props.parameterType}(this, "${name}", {}),`
-      );
-    } else {
-      this.code.indent(
-        `${name}: new ${props.parameterType}(this, "${name}", {`
-      );
-
-      propsToRender.forEach(([key, val]) => {
-        const pKey = toCamelCase(key);
-
-        this.code.line(
-          [`${pKey}: `, this.formatParam(pKey, val), `,`].join("")
-        );
-      });
-
-      this.code.unindent(`}),`);
-    }
-  }
-
-  formatParam(name: string, value: unknown): unknown {
-    switch (name) {
-      case "noEcho":
-        return value;
-      case "allowedValues":
-        return Array.isArray(value)
-          ? `[${value.map((v: string) => `"${v}"`).join(", ")}]`
-          : value;
-      default:
-        return typeof value === "string" ? `"${value}"` : value;
-    }
-  }
-
-  shouldSkipParamProp(key: string, val: unknown): boolean {
-    const keysToSkip = ["parameterType", "comment"];
-
-    if (keysToSkip.includes(key)) return true;
-
-    // Special cases
-
-    if (key === "type" && val === "String") return true;
-
-    return false;
   }
 }
 
